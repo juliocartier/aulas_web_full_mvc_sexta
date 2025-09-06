32039ca331155ee5bda78db1a7973a3b479f3658
JULIO CARTIER MAIA GOMES <02060713374@professores.unifanor.edu.br>
2025-09-05 22:17:13 -0300

    Adicionando os metodos de atualizar e deletar

--- primeiro_projeto/src/main/java/com/example/primeiro_projeto/controller/ProdutoController.java
+++ primeiro_projeto/src/main/java/com/example/primeiro_projeto/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -21,5 +22,29 @@ public class ProdutoController {
     @PostMapping
     public Produto adicionar(@RequestBody Produto produto){
         return produtoRepository.save(produto);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, 
+                             @RequestBody Produto produtoAtualizado){
+        Optional<Produto> produtoExistente = produtoRepository.findById(id);
+        if (produtoExistente.isPresent()){
+            Produto produto = produtoExistente.get();
+            produto.setNome(produtoAtualizado.getNome());
+            produto.setPreco(produtoAtualizado.getPreco());
+            return produtoRepository.save(produto);
+        } else {
+            throw new RuntimeException("Produto nao encontrado = " + id );
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletar(@PathVariable Long id){
+        if (produtoRepository.existsById(id)){
+            produtoRepository.deleteById(id);
+            return "Produto com id " + id + " deletado";
+        } else {
+            return "Produto com id " + id + " nao encontrado";
+        }
     }
 }
